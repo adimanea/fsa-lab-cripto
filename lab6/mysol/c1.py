35a6a7b93f5a39305db26ee9746992a9d7fc1407
adimanea <adrianmanea@fastmail.fm>
2021-01-25 11:21:31 +0200

    + cerința

--- lab6/mysol/c1.py
+++ lab6/mysol/c1.py
@@ -1,3 +1,5 @@
+# Cifrul Caesar pe blocuri, în care cheia de bloc este suma indicilor literelor din bloc.
+
 alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
            's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
